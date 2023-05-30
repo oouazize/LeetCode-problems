9a22309fb773ba8a97d61c415ce757dddff7eaf3
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-05-30 09:38:40 +0100

    Time: 3 ms (92.02%), Space: 10.1 MB (78.76%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-        vector<int> res;
-        res.reserve(nums1.size());
+        int index = 0;
         for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
             if (nums1[i] == nums2[j]) {
-                res.push_back(nums1[i]);
+                nums1[index++] = nums1[i];
                 i++; j++;
             }
             else if (nums1[i] < nums2[j]) i++;
             else j++;
         }
-        return res;
+        return vector<int>(nums1.begin(), nums1.begin()+index);
     }
 };
