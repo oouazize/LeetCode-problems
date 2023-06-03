12e49985f32c2c8525459d69fd50e67e0bb930c1
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-06-03 13:47:31 +0100

    Time: 3 ms (64.91%), Space: 7.2 MB (58.32%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if (str1.size() < str2.size()) swap(str1, str2);
-        int R = str2.size();
-        int A = str1.size();
-        int B = R;
-        R = A % B;
-        while (R) {
-            A = B;
-            B = R;
-            R = A % B;
-        }
-        return (str1 + str2 == str2 + str1) ? str1.substr(0, B) : "";
+        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.size(), str2.size())) : "";
     }
 };
