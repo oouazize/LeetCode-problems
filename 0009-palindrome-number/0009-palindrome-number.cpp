6d7b4c042b6e4c6d6f5ed82a73d28272daea2af8
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-06-09 20:31:22 +0100

    Time: 8 ms (90.51%), Space: 5.9 MB (48.57%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0) return false;
-        int size = 0;
-        for (int n = x; n > 0; n/= 10) size++;
+        int res = x;
+        unsigned int rev = 0;
         while (x) {
-            int r = x % 10;
-            int l = x / pow(10, size - 1);
-            if (l != r) return false;
+            int rem = x % 10;
+            rev = rev*10 + rem;
             x /= 10;
-            size -= 2;
-            if (size < 0) break;
-            x %= (int)(pow(10, size));
         }
-        return true;
+        if (res == rev) return true;
+        return false;
     }
 };
