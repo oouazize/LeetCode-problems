eccfbc243135c7ea5e17156dce89cea7a25bed7f
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-06-10 16:59:46 +0100

    Time: 224 ms (29.12%), Space: 80.5 MB (21.33%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int, int> list;
-        for (int i = 0; i < nums.size(); i++) {
-            if (list.count(nums[i])) {
-                if (abs(i - list[nums[i]]) <= k) return true;
-                list[nums[i]] = i;
-            }
-            else
-                list.insert({nums[i], i});
+        set<int> hashTable;
+        int oldSize;
+        for (int index = 0; index < nums.size() && index <= k; index++) {
+            oldSize = hashTable.size();
+            hashTable.insert(nums[index]);
+            if (hashTable.size() == oldSize) return true;
+        }
+        for (int index = k + 1; index < nums.size(); index++) {
+            hashTable.erase(nums[index - k - 1]);
+            oldSize = hashTable.size();
+            hashTable.insert(nums[index]);
+            if (hashTable.size() == oldSize) return true;
         }
         return false;
     }
