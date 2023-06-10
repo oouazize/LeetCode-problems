bf4ca88e95b35754de27dac91e2b26709ae75b5f
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-06-10 17:04:30 +0100

    Time: 231 ms (27.77%), Space: 80.4 MB (21.33%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         set<int> list;
+        int oldSize;
         for (int index = 0; index < nums.size() && index <= k; index++) {
-            if (list.count(nums[index])) return true;
+            oldSize = list.size();
             list.insert(nums[index]);
+            if (list.size() == oldSize) return true;
         }
         for (int index = k + 1; index < nums.size(); index++) {
             list.erase(nums[index - k - 1]);
-            if (list.count(nums[index])) return true;
+            oldSize = list.size();
             list.insert(nums[index]);
+            if (list.size() == oldSize) return true;
         }
         return false;
     }
