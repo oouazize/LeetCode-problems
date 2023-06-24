dfa55c67c0b7314a9a626554cabfc0d03966f5e1
oussama ouazize <93908021+oouazize@users.noreply.github.com>
2023-06-24 19:37:42 +0100

    Time: 23 ms (68.13%), Space: 23.1 MB (94.75%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
-            s[i] ^= s[j];
-            s[j] ^= s[i];
-            s[i] ^= s[j];
-        }
+        reverse(s.begin(), s.end());
     }
 };
